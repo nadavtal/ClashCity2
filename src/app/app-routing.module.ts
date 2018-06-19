fe4f9b0097fcf009cf1b79b0f4ea29314061bbc7
nadav almagor <nadavtalalmagor@gmail.com>
2018-06-19 16:35:26 +0300

    users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,20 @@ import { PlayersComponent } from './players/players.component';
 import { DetailsComponent } from './details/details.component'
 import { BattlesComponent } from './battles/battles.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PlayersComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  {
+    path: 'battles',
+    component: BattlesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
